39c4d2de94188c15a82d207e9adf78b2846e4f69
unknown <pedrero888@gmail.com>
2022-05-22 12:17:28 -0700

    changes

--- cypress/integration/my_first_test.js
+++ cypress/integration/my_first_test.js
@@ -2,9 +2,20 @@
 
 
 it("google test", function() {
+  
   cy.visit("https://google.com")
   cy.get('.gLFyf').type('Automation Step by Step{enter}')
   cy.contains('Videos').click()
+
 })
 
-// "https://www.youtube.com/watch?v=vZamodE6hrs&list=PLhW3qG5bs-L9LTfxZ5LEBiM1WFfvX3dJo&index=7"
+it.only("login test", function() {
+
+  cy.visit("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+  cy.get('#txtUsername').type("Admin")
+  cy.get('#txtPassword').type("admin123")
+  cy.get('#btnLogin').click();
+
+})
+
+// https://www.youtube.com/watch?v=vZamodE6hrs&list=PLhW3qG5bs-L9LTfxZ5LEBiM1WFfvX3dJo&index=8 
